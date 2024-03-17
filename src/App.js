1c5634c058785fa6e87a3393dc2d7470127d0c29
imAshutoshGupta <aguptaworkspace@gmail.com>
2024-03-17 12:01:49 +0530

    further update to the project

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import Cardlist from './Cardlist';
 import Search from './Search';
 import productsData from './products.json'
 import './card.css'
+import { useState } from 'react';
 
 function App() {
+  const [products, setProducts] = useState(productsData)
+  const searchProduct = (productName) => {
+    console.log(productName)
+    let x = productsData.filter(product=>
+      product.title.toLowerCase().includes(productName.toLowerCase()))
+    setProducts(x)
+  }
+  
   return ( <>
-  <Search/>
+  <Search onSearch={searchProduct}/>
   <div className='card'>
-  <Cardlist products={productsData}/>
+  <Cardlist products={products}/>
   </div>
   </>
   )
 }
-
+                                        
 export default App;
--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,37 @@
+import { useRef, useState } from 'react'
 import './search.css'
-function Search(){
-    return <div>
-        <form action="">
-        <input type='text' placeholder='Product name'></input>
-        <button type='submit'>Search</button>
-        </form>
+// function Search(){
+//     const productName = useRef()
+//     const submitForm = (event) => {
+//         event.preventDefault()
+//         console.log(productName.current.value);
+//     }
+//     return <div>
+//         <form action="" onSubmit={submitForm}>
+//         <input type='text' placeholder='Product name' ref={productName}/>
+//         <button type='submit'>Search</button>
+//         </form>
+//         </div>
+// }
+// export default Search
+export default function Search(props) {
+    let [productName, setProductName] = useState('')
+    const submitForm = (event) => {
+        event.preventDefault()
+        console.log(productName)
+        props.onSearch(productName)
+    }
+
+    const changeProductName = (event) => {
+        setProductName(event.target.value)
+    }
+
+    return (
+        <div>
+            <form onSubmit={submitForm}>
+                <input type='text' placeholder='Product name' onChange={changeProductName} />
+                <button type='submit'>Search</button>
+            </form>
         </div>
+    )
 }
-export default Search
